1d4b82ee2aa9184dbd99c07f916ce2115c655db5
전태헌 <brgndyy@gmail.com>
2025-12-30 16:03:11 +0900

    granite.config.ts 수정

--- marriage-quiz/granite.config.ts
+++ marriage-quiz/granite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "@apps-in-toss/web-framework/config";
 export default defineConfig({
   appName: "marriage-quiz",
   brand: {
-    displayName: "예비부부 모의고사", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
-    primaryColor: "#2563EB", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: "https://static.toss.im/tds/favicon/favicon-32x32.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    displayName: "예비부부 모의고사",
+    primaryColor: "#2563EB",
+    icon: "https://static.toss.im/appsintoss/6371/ac90029e-49cb-4577-9493-7a584a387b86.png",
   },
   web: {
     host: "localhost",
